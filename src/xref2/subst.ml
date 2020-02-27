c93e75a9360a26ff252095842972d8e333fe4660
Jules Aguillon <juloo.dsi@gmail.com>
2020-02-27 15:34:23 +0100

    wip2

--- src/xref2/subst.ml
+++ src/xref2/subst.ml
@@ -102,17 +102,6 @@ let compose_delayed' compose v s =
   | DelayedSubst (s', v) -> DelayedSubst (compose s s', v)
   | NoSubst v -> DelayedSubst (s, v)
 
-let compose_path ~find resolve _key (a : ([> `Local of 'a ] as 'path) option) b =
-  match a, b with
-  | Some (`Local a), Some b ->
-    begin match find a with
-    | Some _ as b' -> b'
-    | None -> Some (resolve b)
-    end
-  | Some _, Some b -> Some (resolve b)
-  | None, (Some _ as path) -> path
-  | path, None -> (path :> 'path option)
-
 let rec resolved_module_path :
     t -> Cpath.Resolved.module_ -> Cpath.Resolved.module_ =
  fun s p ->
@@ -789,15 +778,18 @@ and compose : t -> t -> t =
     | None -> a
   in
   fun a b ->
-  { module_ = ModuleMap.merge (compose_path ~find:(fun id -> ModuleMap.find_opt id b.module_) (resolved_module_path a)) a.module_ b.module_
-  ; module_type = ModuleTypeMap.merge override a.module_type b.module_type
-  ; type_ = TypeMap.merge override a.type_ b.type_
-  ; class_type = ClassTypeMap.merge override a.class_type b.class_type
+  let compose_map ~add ~fold resolve field =
+    fold (fun key path acc -> add key (resolve b path) acc) (field a) (field b)
+  in
+  { module_ = ModuleMap.(compose_map ~add ~fold resolved_module_path) (fun t -> t.module_)
+  ; module_type = ModuleTypeMap.(compose_map ~add ~fold resolved_module_type_path) (fun t -> t.module_type)
+  ; type_ = TypeMap.(compose_map ~add ~fold resolved_type_path) (fun t -> t.type_)
+  ; class_type = ClassTypeMap.(compose_map ~add ~fold resolved_class_type_path) (fun t -> t.class_type)
   ; type_replacement = TypeMap.merge override a.type_replacement b.type_replacement
-  ; ref_module = ModuleMap.merge override a.ref_module b.ref_module
+  ; ref_module = ModuleMap.(compose_map ~add ~fold resolved_module_reference) (fun t -> t.ref_module)
   ; ref_module_type = ModuleTypeMap.merge override a.ref_module_type b.ref_module_type
-  ; ref_type = TypeMap.merge override a.ref_type b.ref_type
-  ; ref_class_type = ClassTypeMap.merge override a.ref_class_type b.ref_class_type
+  ; ref_type = TypeMap.(compose_map ~add ~fold resolved_type_reference) (fun t -> t.ref_type)
+  ; ref_class_type = ClassTypeMap.(compose_map ~add ~fold resolved_class_signature_reference) (fun t -> t.ref_class_type)
   ; id_any = IdentMap.merge override a.id_any b.id_any
   }
 
